9260078a609b677648bbb013989f5ebf8df8110f
Josue Oroya <joshuacba08@gmail.com>
2021-03-02 23:59:20 -0300

    se integran pruebas con enzyme

--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,14 +1,23 @@
-import { render } from "@testing-library/react";
+// import { render } from "@testing-library/react";
 import PrimeraApp from "../PrimeraApp";
+import { shallow } from 'enzyme';
+import '@testing-library/jest-dom';
 
 describe('Pruebas en <PrimeraApp />', () => {
     
-    test('Debe mostrar el mensaje "Hola, soy Josué"', () => {
+    // test('Debe mostrar el mensaje "Hola, soy Josué"', () => {
+    //     const saludo = "Hola, soy Josué";
+    //     const { getByText } = render( <PrimeraApp saludo ={ saludo }/>)
+    //     expect( getByText(saludo) ).toBeInTheDocument(); 
+    // })
+    test('Debe de mostrar el compotente <PrimeraAPP /> correctamente', () => {
+        
+        const saludo = "Hola, soy Josué"
+        const wrapper = shallow( <PrimeraApp saludo ={ saludo } /> );
 
-        const saludo = "Hola, soy Josué";
-
-        const { getByText } = render( <PrimeraApp saludo ={ saludo }/>)
-
-        expect( getByText(saludo) ).toBeInTheDocument(); 
+        expect( wrapper ).toMatchSnapshot();
     })
+    
+
+
 })
